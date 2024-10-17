8b201a7341ce4dee8b94aa334961579504a02f59
Аня Мур <77534172+mura4k@users.noreply.github.com>
2024-10-17 20:13:36 +0700

    Changed tokenization and comparison algorithm for error-search optimization

--- scripts/analysis/highlight_mistakes.py
+++ scripts/analysis/highlight_mistakes.py
@@ -1,38 +1,87 @@
 from difflib import SequenceMatcher
 import os
 import json
+import re
+import sqlite3
+from collections import Counter
+from typing import Dict, List
 
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+DB_DIR = os.path.join(BASE_DIR, 'data', 'tibetan_syllables.db')
+
+def search_in_db(search_word: str):
+    # Path to SQLite database file
+    db_path = DB_DIR
+
+    table_name = 'tibetan_syllables'
+    syllable_column = 'Transliteration'
+    count_column = 'SymbolCount'
+
+    search_word = search_word
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Query to check if the word exists in the column
+    query = f"SELECT {count_column} FROM {table_name} WHERE {syllable_column} = ?"
+    try:
+        # Execute the query with the search word
+        cursor.execute(query, (search_word,))
+        # Fetch one result (since we only need to know if it exists)
+        result = cursor.fetchone()
+        if result:
+            conn.close()
+            return result[0]
+
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+    conn.close()
+    return -1
 
 
-def find_mistakes(ocr_data, man_data):
-    for key in ocr_data.keys():
-        if key in man_data:
-            result = []
-            if isinstance(ocr_data[key], str):
-                ocr_data[key] = [ocr_data[key]]
-            if isinstance(man_data[key], str):
-                man_data[key] = [man_data[key]]
+def find_mistakes(ocr_data: Dict[int, List], man_data: Dict[int, List], syllable_mistakes) -> List[tuple]:
+    all_mistakes = []  # List with tuples containing mistakes
 
-            if not ocr_data[key]:
-                ocr_data[key] = ["<mistake></mistake>"]
-                man_data[key] = ["<error-prone>" + ' '.join(man_data[key]) + "</error-prone>"]
+    for key in ocr_data.keys():  # Iterate through every string
 
-            for i in range(len(man_data[key])):
-                seq = SequenceMatcher(a=ocr_data[key][i], b=man_data[key][i])
-                if seq.ratio() != 1:
-                    result.append((ocr_data[key][i], man_data[key][i]))
-                    ocr_data[key][i] = "<mistake>" + ocr_data[key][i] + "</mistake>"
-                    man_data[key][i] = "<error-prone>" + man_data[key][i] + "</error-prone>"
+        # If the first string is empty, it is str type, so it is converted to empty list
+        if isinstance(ocr_data[key], str):
+            ocr_data[key] = [ocr_data[key]]
+        if isinstance(man_data[key], str):
+            man_data[key] = [man_data[key]]
 
-            # Join the lists back into a single string for each key
-            ocr_data[key] = " ".join(ocr_data[key])
-            man_data[key] = " ".join(man_data[key])
-    return result
+        if not ocr_data[key]:  # Handle not recognised first string
+            ocr_data[key] = ["<mistake></mistake>"]
+            man_data[key] = ["<error-prone>" + ' '.join(man_data[key]) + "</error-prone>"]
+
+        for i in range(len(man_data[key])):
+            seq = SequenceMatcher(a=ocr_data[key][i].replace(' ', ''), b=man_data[key][i].replace(' ', ''))
+            if seq.ratio() != 1:
+                # pattern = re.compile(r"[a-zA-Z']+")
+                # try:
+                #     if pattern.match(man_data[key][i])[0] == pattern.match(ocr_data[key][i])[0]:
+                #
+                #     print()
+                # except:
+                #     print()
+                syllable_number = search_in_db(man_data[key][i])
+                if syllable_number != -1:
+                    syllable_mistakes[syllable_number] += 1
+                all_mistakes.append((ocr_data[key][i], man_data[key][i]))
+                ocr_data[key][i] = "<mistake>" + ocr_data[key][i] + "</mistake>"
+                man_data[key][i] = "<error-prone>" + man_data[key][i] + "</error-prone>"
+
+        # Join the lists back into a single string for each key
+        ocr_data[key] = " ".join(ocr_data[key])
+        man_data[key] = " ".join(man_data[key])
+    return all_mistakes
 
 
 def main():
+    syllable_mistakes = Counter()
     for file_number in list(range(2, 51)) + list(range(471, 501)):
         ocr_file_path = os.path.join(BASE_DIR, 'data', 'ocr_texts', 'processed', f'{file_number}_ocr.json')
         man_file_path = os.path.join(BASE_DIR, 'data', 'manually_recognised_texts', 'processed',
@@ -46,17 +95,26 @@ def main():
 
         if os.path.exists(ocr_file_path) and os.path.exists(man_file_path):
 
-            result_dir = os.path.join(BASE_DIR, 'results', str(file_number))
+            result_dir = os.path.join(BASE_DIR, 'results1', str(file_number))
             os.makedirs(result_dir, exist_ok=True)
-            mistakes = find_mistakes(ocr_data, man_data)
+            mistakes = find_mistakes(ocr_data, man_data, syllable_mistakes)
+            print(mistakes)
+            print(ocr_data)
+            print(man_data)
 
-            with open(os.path.join(result_dir, f'{file_number}_ocr.json'), 'w', encoding='utf-8') as ocr_result_file:
-                json.dump(ocr_data, ocr_result_file, ensure_ascii=False, indent=4)
 
-            with open(os.path.join(result_dir, f'{file_number}_man.json'), 'w', encoding='utf-8') as man_result_file:
-                json.dump(man_data, man_result_file, ensure_ascii=False, indent=4)
+
+            # with open(os.path.join(result_dir, f'{file_number}_ocr.json'), 'w', encoding='utf-8') as ocr_result_file:
+            #     json.dump(ocr_data, ocr_result_file, ensure_ascii=False, indent=4)
+            #
+            # with open(os.path.join(result_dir, f'{file_number}_man.json'), 'w', encoding='utf-8') as man_result_file:
+            #     json.dump(man_data, man_result_file, ensure_ascii=False, indent=4)
 
             print(f'Processed file number {file_number}')
+    mistakes_count = syllable_mistakes.total()
+    for i in syllable_mistakes.keys():
+        syllable_mistakes[i] /= mistakes_count
+    print(syllable_mistakes)
 
 
 if __name__ == "__main__":
--- scripts/preprocessing/text_alignment.py
+++ scripts/preprocessing/text_alignment.py
@@ -1,134 +1,131 @@
 from difflib import SequenceMatcher
-from typing import List
+from typing import List, Dict, Tuple
 import os
 import json
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+counter = 0
+
+def string_tokenizer(text: str) -> List[str]:
+    text = text.replace('  ', ' ')
+    result = []
+    shad = '/'
+    igo = '@'
+    prev = ''
+
+    for char in text:
+        if char == ' ':
+            if prev:
+                result.append(prev)
+            prev = ''
+        elif char in {shad, igo}:
+            if prev and prev[-1] == char:
+                prev += char
+            else:
+                if prev:
+                    result.append(prev)
+                prev = char
+        else:
+            if prev and prev[-1] in {shad, igo}:
+                result.append(prev)
+                prev = char
+            else:
+                prev += char
+
+    if prev:
+        result.append(prev)
+
+    return result
 
 
-def adjust_ocr_json(file_number: int):
-    ocr_file_path = os.path.join(BASE_DIR, 'data', 'ocr_texts', 'processed', f'{file_number}_ocr.json')
-    man_file_path = os.path.join(BASE_DIR, 'data', 'manually_recognised_texts', 'processed', f'{file_number}_man.json')
-
-    with open(ocr_file_path, 'r', encoding='utf-8') as ocr_file:
-        ocr_data = json.load(ocr_file)
-
-    with open(man_file_path, 'r', encoding='utf-8') as man_file:
-        man_data = json.load(man_file)
-
+def tokenize_files(ocr_data: Dict, man_data: Dict) -> Tuple[Dict, Dict]:
     if len(ocr_data) == len(man_data) - 1:
         new_ocr_data = {"1": ""}
         for key in sorted(ocr_data.keys(), key=int):
             new_ocr_data[str(int(key) + 1)] = ocr_data[key]
+        ocr_data = new_ocr_data
 
-        with open(ocr_file_path, 'w', encoding='utf-8') as ocr_file:
-            json.dump(new_ocr_data, ocr_file, ensure_ascii=False, indent=4)
+    for string_num in ocr_data.keys():
+        ocr_data[string_num] = string_tokenizer(ocr_data[string_num])
+    for string_num in man_data.keys():
+        man_data[string_num] = string_tokenizer(man_data[string_num])
 
-
-def glue_words_with_shad(text_list: List[str]) -> List[str]:
-    flag = 0
-    result = []
-    for i in range(len(text_list)):
-        if flag:
-            result[-1] += ' ' + text_list[i]
-            flag = 0
-            continue
-        elif text_list[i].startswith('/') and text_list[i].endswith('/'):
-            if result:
-                result[-1] += ' ' + text_list[i]
-            else:
-                result.append(text_list[i])
-            flag = 1
-            continue
-        elif text_list[i].startswith('/'):
-            if result:
-                result[-1] += ' ' + text_list[i]
-            else:
-                result.append(text_list[i])
-            continue
-        elif text_list[i].endswith('/'):
-            try:
-                text_list[i + 1] = text_list[i] + ' ' + text_list[i + 1]
-            except IndexError:
-                result.append(text_list[i])
-            continue
-        result.append(text_list[i])
-    return result
+    return ocr_data, man_data
 
 
-def level_word_count(auto_text, manual_text):
-    for i in range(max(len(auto_text), len(manual_text))):
-        try:
-            seq1 = SequenceMatcher(a=auto_text[i], b=manual_text[i]).ratio()
-            if i != len(auto_text) - 1:
-                seq2 = SequenceMatcher(a=auto_text[i] + auto_text[i + 1], b=manual_text[i]).ratio()
-            else:
-                seq2 = 0
-            seq3 = SequenceMatcher(a=auto_text[i], b=manual_text[i] + manual_text[i + 1]).ratio()
-            if seq2 > seq1 and seq2 > seq3:
-                auto_text[i] = auto_text[i] + ' ' + auto_text[i + 1]
-                for j in range(i+1, len(auto_text) - 1):
-                    auto_text[j] = auto_text[j + 1]
-                auto_text.pop()
-            if seq3 > seq2 and seq3 > seq1:
-                manual_text[i] = manual_text[i] + ' ' + manual_text[i + 1]
-                for j in range(i+1, len(manual_text) - 1):
-                    manual_text[j] = manual_text[j + 1]
-                manual_text.pop()
-        except IndexError:
-            break
+def level_word_count(auto_text, manual_text, file_number, string_number):
+    min_length = min(len(auto_text), len(manual_text))
+
+    # If the length of the shorter text is 0 (first line in OCR is not recognised, end leveling)
+    if min_length == 0:
+        return
+
+    i = 0
+    while i < min_length:
+        seq_base = SequenceMatcher(a=auto_text[i], b=manual_text[i]).ratio()
+
+        seq_ocr_cat = SequenceMatcher(a=auto_text[i] + (auto_text[i + 1] if i + 1 < len(auto_text) else ''),
+                                      b=manual_text[i]).ratio()
+
+        seq_man_cat = SequenceMatcher(a=auto_text[i],
+                                      b=manual_text[i] + (
+                                          manual_text[i + 1] if i + 1 < len(manual_text) else '')).ratio()
+
+        if seq_ocr_cat > seq_base and seq_ocr_cat > seq_man_cat:
+            auto_text[i] = auto_text[i] + ' ' + auto_text[i + 1]
+            del auto_text[i + 1]
+
+        elif seq_man_cat > seq_base and seq_man_cat > seq_ocr_cat:
+            manual_text[i] = manual_text[i] + ' ' + manual_text[i + 1]
+            del manual_text[i + 1]
+
+        else:
+            i += 1  # Only move forward if no concatenation occurred
+        min_length = min(len(auto_text), len(manual_text))
+
+    # Post-process to handle one-off length differences
     if len(auto_text) == len(manual_text) - 1:
-        manual_text[-2] = manual_text[-2] + ' ' + manual_text[-1]
+        manual_text[-2] += ' ' + manual_text[-1]
         manual_text.pop()
-    if len(manual_text) == len(auto_text) - 1:
-        auto_text[-2] = auto_text[-2] + ' ' + auto_text[-1]
+    elif len(manual_text) == len(auto_text) - 1:
+        auto_text[-2] += ' ' + auto_text[-1]
         auto_text.pop()
 
-
-def process_and_save_json(ocr_file_path: str, man_file_path: str) -> (int, int):
-    with open(ocr_file_path, 'r', encoding='utf-8') as file:
-        ocr_data = json.load(file)
-
-    with open(man_file_path, 'r', encoding='utf-8') as file:
-        man_data = json.load(file)
-
-    len_ocr = 0
-    len_man = 0
-
-    for key in ocr_data.keys():
-        if key in man_data:
-            ocr_text = glue_words_with_shad(ocr_data[key].split())
-            man_text = glue_words_with_shad(man_data[key].split())
-
-            level_word_count(ocr_text, man_text)
-
-            ocr_data[key] = ocr_text
-            man_data[key] = man_text
-
-            len_ocr += len(ocr_text)
-            len_man += len(man_text)
-
-    with open(ocr_file_path, 'w', encoding='utf-8') as file:
-        json.dump(ocr_data, file, ensure_ascii=False, indent=4)
-
-    with open(man_file_path, 'w', encoding='utf-8') as file:
-        json.dump(man_data, file, ensure_ascii=False, indent=4)
-
-    return len_ocr, len_man
+    if len(auto_text) != len(manual_text) and len(auto_text) != 0:
+        print()
+        global counter
+        counter += 1
+        print(f"Файл номер {file_number}, строка {string_number}, требует ручной разметки")
+        print(len(auto_text))
+        print(auto_text)
+        print(len(manual_text))
+        print(manual_text)
+        print()
 
 
 def main():
     for file_number in list(range(2, 51)) + list(range(471, 501)):
-        adjust_ocr_json(file_number)
         ocr_file_path = os.path.join(BASE_DIR, 'data', 'ocr_texts', 'processed', f'{file_number}_ocr.json')
-        man_file_path = os.path.join(BASE_DIR, 'data', 'manually_recognised_texts', 'processed', f'{file_number}_man.json')
+        man_file_path = os.path.join(BASE_DIR, 'data', 'manually_recognised_texts', 'processed',
+                                     f'{file_number}_man.json')
 
-        len_ocr, len_man = process_and_save_json(ocr_file_path, man_file_path)
+        with open(ocr_file_path, 'r', encoding='utf-8') as ocr_file:
+            ocr_data = json.load(ocr_file)
+        with open(man_file_path, 'r', encoding='utf-8') as man_file:
+            man_data = json.load(man_file)
 
-        print(f'File number {file_number}')
-        print(f'Length of OCR Text: {len_ocr}')
-        print(f'Total Length of Manual Text: {len_man}')
-        print()
+        
+        ocr_data, man_data = tokenize_files(ocr_data, man_data)
+        for string_num in ocr_data.keys():
+            level_word_count(ocr_data[string_num], man_data[string_num], file_number, string_num)
+
+        with open(ocr_file_path, 'w', encoding='utf-8') as file:
+            json.dump(ocr_data, file, ensure_ascii=False, indent=4)
+        with open(man_file_path, 'w', encoding='utf-8') as file:
+            json.dump(man_data, file, ensure_ascii=False, indent=4)
+    
+    print(counter)
+
 
 
 if __name__ == "__main__":
